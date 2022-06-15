ef12c4fc1418ed9a90aeed00637bf77bb5378771
sdercolin <sder.colin@gmail.com>
2022-06-15 23:09:35 +0900

    Edit project model

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/model/Project.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/model/Project.kt
@@ -3,7 +3,8 @@ package com.sdercolin.vlabeler.model
 import java.io.File
 
 data class Project(
-    val workingPath: File,
-    val samples: List<SampleInfo>,
-    val entriesBySampleName: Map<String, List<Entry>>
+    val workingDirectory: File,
+    val entriesBySampleName: Map<String, List<Entry>>,
+    val appConf: AppConf,
+    val labelerConf: LabelerConf
 )
