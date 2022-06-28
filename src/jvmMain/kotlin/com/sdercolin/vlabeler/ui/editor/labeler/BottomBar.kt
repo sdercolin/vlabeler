21214b3fc29ae444a9b88da271cbae50d5de0fea
sdercolin <sder.colin@gmail.com>
2022-06-29 04:16:32 +0900

    Fix layout of BottomBar

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/editor/labeler/BottomBar.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/editor/labeler/BottomBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -59,6 +60,7 @@ fun BottomBar(state: BottomBarState) {
             }
             Box(
                 Modifier.fillMaxHeight()
+                    .widthIn(min = 85.dp)
                     .clickable { state.openJumpToEntryDialog() }
                     .padding(horizontal = 15.dp)
             ) {
