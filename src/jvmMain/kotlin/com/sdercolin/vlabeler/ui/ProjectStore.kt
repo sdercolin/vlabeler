34837f2323c204782b50c8aade8c74fe1d8340ec
sdercolin <sder.colin@gmail.com>
2022-07-01 23:31:46 +0900

    Format

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/ProjectStore.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/ProjectStore.kt
@@ -1,6 +1,5 @@
 package com.sdercolin.vlabeler.ui
 
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
