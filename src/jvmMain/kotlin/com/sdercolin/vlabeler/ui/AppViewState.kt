8d326c8e3da6850bfb34b19a42a0b5ac0e61ff52
sdercolin <sder.colin@gmail.com>
2022-07-01 15:39:50 +0900

    Rename

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/AppViewState.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/AppViewState.kt
@@ -3,18 +3,18 @@ package com.sdercolin.vlabeler.ui
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.sdercolin.vlabeler.util.backed
+import com.sdercolin.vlabeler.util.cached
 
 class AppViewState(appRecord: AppRecordStore) {
     var isMarkerDisplayed: Boolean by mutableStateOf(true)
 
-    var isPropertyViewDisplayed by appRecord.backed(
-        valueSelector = { it.isPropertyViewDisplayed },
-        valueUpdater = { base, value -> base.copy(isPropertyViewDisplayed = value) }
+    var isPropertyViewDisplayed by appRecord.cached(
+        selector = { it.isPropertyViewDisplayed },
+        updater = { base, value -> base.copy(isPropertyViewDisplayed = value) }
     )
 
-    var isEntryListPinned: Boolean by appRecord.backed(
-        valueSelector = { it.isEntryListPinned },
-        valueUpdater = { base, value -> base.copy(isEntryListPinned = value) }
+    var isEntryListPinned: Boolean by appRecord.cached(
+        selector = { it.isEntryListPinned },
+        updater = { base, value -> base.copy(isEntryListPinned = value) }
     )
 }
--- src/jvmMain/kotlin/com/sdercolin/vlabeler/util/CachedState.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/util/CachedState.kt
@@ -4,23 +4,23 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import kotlin.reflect.KProperty
 
-class ValueBackedState<T, K>(
+class CachedState<T, K>(
     private val backState: MutableState<K>,
-    valueSelector: (K) -> T,
-    private val valueUpdater: (K, T) -> K
+    selector: (K) -> T,
+    private val updater: (K, T) -> K
 ) {
 
-    private val state = mutableStateOf(valueSelector(backState.value))
+    private val state = mutableStateOf(selector(backState.value))
 
     operator fun getValue(thisObj: Any?, property: KProperty<*>) = state.value
 
     operator fun setValue(thisObj: Any?, property: KProperty<*>, value: T) {
         this.state.value = value
-        backState.value = valueUpdater(backState.value, value)
+        backState.value = updater(backState.value, value)
     }
 }
 
-fun <T, K> MutableState<K>.backed(
-    valueSelector: (K) -> T,
-    valueUpdater: (K, T) -> K
-) = ValueBackedState(this, valueSelector, valueUpdater)
+fun <T, K> MutableState<K>.cached(
+    selector: (K) -> T,
+    updater: (K, T) -> K
+) = CachedState(this, selector, updater)
