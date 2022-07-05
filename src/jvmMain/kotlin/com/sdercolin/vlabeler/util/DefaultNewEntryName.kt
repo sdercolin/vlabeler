9e0f6d463fa61bdf1f02a73d95f4fd203fcb9573
sdercolin <sder.colin@gmail.com>
2022-07-06 03:46:04 +0900

    Fix getDefaultNewEntryName

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/util/DefaultNewEntryName.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/util/DefaultNewEntryName.kt
@@ -3,8 +3,8 @@ package com.sdercolin.vlabeler.util
 fun getDefaultNewEntryName(sourceEntryName: String, allEntryNames: List<String>, allowDuplicate: Boolean): String {
     if (allowDuplicate) return sourceEntryName
 
-    val countMatch = Regex("\\d+\$").find(sourceEntryName)
-    val body = if (countMatch == null) sourceEntryName else sourceEntryName.take(countMatch.range.first)
+    val countMatch = Regex("(?<=_)\\d+\$").find(sourceEntryName)
+    val body = if (countMatch == null) sourceEntryName + "_" else sourceEntryName.take(countMatch.range.first)
     var count = countMatch?.value?.toInt() ?: 1
     while (true) {
         count++
