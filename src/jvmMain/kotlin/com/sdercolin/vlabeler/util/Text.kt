4f60aca0631a67cda3cda64b606c31490fcfa757
sdercolin <sder.colin@gmail.com>
2022-08-22 21:49:44 +0900

    Fix trimming in float input box

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/util/Text.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/util/Text.kt
@@ -1,5 +1,5 @@
 package com.sdercolin.vlabeler.util
 
 fun Float.toStringTrimmed(): String = this.toString()
-    .runIf(this.toString().contains(".")) { trim('0') }
+    .runIf(this.toString().contains(".")) { trimEnd('0') }
     .trim('.')
