bb93d12300543439c58d1c5a5aa2cb89edcaf0fd
sdercolin <sder.colin@gmail.com>
2022-08-23 13:53:29 +0900

    Fix lab output float max value

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/util/Math.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/util/Math.kt
@@ -1,7 +1,7 @@
 package com.sdercolin.vlabeler.util
 
 import kotlin.math.pow
-import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
-fun Float.roundToDecimalDigit(n: Int): Float = 10.0.pow(n).let { times(it).roundToInt().toFloat().div(it.toFloat()) }
-fun Double.roundToDecimalDigit(n: Int): Double = 10.0.pow(n).let { times(it).roundToInt().toFloat().div(it) }
+fun Float.roundToDecimalDigit(n: Int): Float = 10.0.pow(n).let { times(it).roundToLong().toFloat().div(it.toFloat()) }
+fun Double.roundToDecimalDigit(n: Int): Double = 10.0.pow(n).let { times(it).roundToLong().toFloat().div(it) }
