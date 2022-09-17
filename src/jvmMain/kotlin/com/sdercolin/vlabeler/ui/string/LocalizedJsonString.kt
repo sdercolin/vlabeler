ffc0e95cb57b9cc53d4af31d2dc8d09b37c24ac5
sdercolin <sder.colin@gmail.com>
2022-09-18 00:49:46 +0900

    Add docs

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/string/LocalizedJsonString.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/string/LocalizedJsonString.kt
@@ -17,6 +17,15 @@ import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
+/**
+ * A localized string that stored in JSON, used by labelers and plugins.
+ * Two types of values are supported:
+ * 1. A string value, which is a string that is used by [Language.default].
+ * 2. A map of language code to string value, e.g. {"en": "Hello", "zh": "你好"}.
+ *   An entry is used when the current language code starts with the language code in the entry.
+ *   e.g. If the current language code is "en-US", the entry with key "en" is used.
+ *   Note that a map without an entry for [Language.default] is not allowed.
+ */
 @Serializable(with = LocalizedJsonStringSerializer::class)
 data class LocalizedJsonString(
     val localized: Map<String, String>,
