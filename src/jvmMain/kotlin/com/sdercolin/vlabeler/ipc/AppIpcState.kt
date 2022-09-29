28a7bac30719345b6cbdb87292b1e8fc5fb0021c
sdercolin <sder.colin@gmail.com>
2022-09-29 20:13:46 +0900

    Implement Heartbeat

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/ipc/AppIpcState.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/ipc/AppIpcState.kt
@@ -1,13 +1,14 @@
 package com.sdercolin.vlabeler.ipc
 
+import com.sdercolin.vlabeler.ipc.request.HeartbeatRequest
 import com.sdercolin.vlabeler.ipc.request.IpcRequest
+import com.sdercolin.vlabeler.ipc.response.HeartbeatResponse
 import com.sdercolin.vlabeler.ipc.response.IpcResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.serialization.encodeToString
 
 interface AppIpcState {
 
@@ -23,12 +24,19 @@ class AppIpcStateImpl(private val scope: CoroutineScope) : AppIpcState {
     override val ipcRequestFlow: MutableSharedFlow<IpcRequest> = MutableSharedFlow<IpcRequest>()
 
     init {
-        ipcRequestFlow.onEach { println(jsonForIpc.encodeToString(it)) }.launchIn(scope)
+        ipcRequestFlow.onEach(::handleRequest).launchIn(scope)
         ipcServer.bind()
         ipcServer.startReceive(ipcRequestFlow)
     }
 
     override fun response(response: IpcResponse) {
         ipcServer.send(response)
+    }
+
+    private fun handleRequest(request: IpcRequest) {
+        val response = when (request) {
+            is HeartbeatRequest -> HeartbeatResponse(request.sentAt, System.currentTimeMillis())
+        }
+        response(response)
     }
 }
