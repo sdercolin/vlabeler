048616e842aeaa5f0f7f8c071aa9dfd8b0556a10
sdercolin <sder.colin@gmail.com>
2022-10-21 00:57:40 +0900

    Remove TODO comment

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/model/AppConf.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/model/AppConf.kt
@@ -266,7 +266,7 @@ data class AppConf(
         @Serializable
         @Immutable
         enum class Target(override val stringKey: Strings) : LocalizedText {
-            // TODO: rename serial names using PascalCase
+            
             @SerialName("None")
             None(Strings.PreferencesEditorScissorsActionTargetNone),
 
