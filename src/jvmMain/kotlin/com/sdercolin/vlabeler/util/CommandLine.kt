6b277af879d6a57a843a4d036fe602fc518a8ea5
sdercolin <sder.colin@gmail.com>
2022-10-22 03:29:35 +0900

    Fix CommandLine

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/util/CommandLine.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/util/CommandLine.kt
@@ -1,10 +1,17 @@
 package com.sdercolin.vlabeler.util
 
+import com.sdercolin.vlabeler.env.isWindows
+
 object CommandLine {
 
     @JvmStatic
-    fun execute(args: Array<String>): String {
-        val process = ProcessBuilder(*args)
+    fun execute(args: List<String>): String {
+        val command = if (isWindows) {
+            listOf(args.joinToString(" ") { "\"$it\"" })
+        } else {
+            args
+        }
+        val process = ProcessBuilder(command)
             .redirectErrorStream(true)
             .start()
         process.waitFor()
