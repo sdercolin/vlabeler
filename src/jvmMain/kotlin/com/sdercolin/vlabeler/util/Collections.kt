9018d1285ea9df37cec6256f7008042e76896484
sdercolin <sder.colin@gmail.com>
2023-03-22 00:51:47 +0900

    Remove unused function

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/util/Collections.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/util/Collections.kt
@@ -10,21 +10,5 @@ fun <T> List<T>.getNextOrNull(predict: (T) -> Boolean): T? {
     return getOrNull(index + 1)
 }
 
-fun <T> Iterable<T>.splitAveragely(count: Int): List<List<T>> {
-    val length = count()
-    val remaining = length % count
-    val divided = length / count
-    val subSizes = (0 until count).map { divided + if (it < remaining) 1 else 0 }
-    var rest = toList()
-    val result = mutableListOf<List<T>>()
-    repeat(count) { i ->
-        val subSize = subSizes[i]
-        val subList = rest.take(subSize)
-        rest = rest.drop(subSize)
-        result += subList
-    }
-    return result
-}
-
 fun <K, V> Map<K, V?>.getNullableOrElse(key: K, defaultValue: () -> V): V? =
     if (containsKey(key)) get(key) else defaultValue()
