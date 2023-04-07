e90b0879472e7e87dd0fe9d9ba3515c37a7e0aec
sdercolin <sder.colin@gmail.com>
2023-04-07 17:46:42 +0900

    Add Rainbow palette

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/model/palette/ColorPaletteDefinition.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/model/palette/ColorPaletteDefinition.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 import com.sdercolin.vlabeler.ui.theme.DarkGray
 import com.sdercolin.vlabeler.ui.theme.White
 import com.sdercolin.vlabeler.util.argbHexString
+import com.sdercolin.vlabeler.util.rgbHexString
 import kotlinx.serialization.Serializable
 
 /**
@@ -40,61 +41,72 @@ data class ColorPaletteDefinition(
                 name = "Plain",
                 items = listOf(
                     Item(White.copy(alpha = 0f).argbHexString, 0f),
-                    Item(White.argbHexString, 1f),
+                    Item(White.rgbHexString, 1f),
                 ),
             ),
             ColorPaletteDefinition(
                 name = "Reversed",
                 items = listOf(
-                    Item(White.argbHexString, 0f),
-                    Item(DarkGray.argbHexString, 1f),
+                    Item(White.rgbHexString, 0f),
+                    Item(DarkGray.rgbHexString, 1f),
                 ),
             ),
             ColorPaletteDefinition(
                 name = "Foggy",
                 items = listOf(
                     Item("#00C76504", 0f),
-                    Item("#FFC76504", 5f),
-                    Item(White.argbHexString, 1f),
+                    Item("#C76504", 5f),
+                    Item(White.rgbHexString, 1f),
                 ),
             ),
             ColorPaletteDefinition(
                 name = "Snowy",
                 items = listOf(
                     Item("#00163EAB", 0f),
-                    Item("#FF163EAB", 5f),
-                    Item(White.argbHexString, 1f),
+                    Item("#163EAB", 5f),
+                    Item(White.rgbHexString, 1f),
                 ),
             ),
             ColorPaletteDefinition(
                 name = "Dawn",
                 items = listOf(
-                    Item(Color.Black.argbHexString, 0f),
-                    Item("#FF020724", 2f),
-                    Item("#FF0286AF", 4f),
-                    Item("#FFBFCAB8", 2f),
-                    Item("#FFE6AAAB", 1f),
-                    Item(White.argbHexString, 0.5f),
+                    Item(Color.Black.rgbHexString, 0f),
+                    Item("#020724", 2f),
+                    Item("#0286AF", 4f),
+                    Item("#BFCAB8", 2f),
+                    Item("#E6AAAB", 1f),
+                    Item(White.rgbHexString, 0.5f),
                 ),
             ),
             ColorPaletteDefinition(
                 name = "Sunset",
                 items = listOf(
-                    Item(Color.Black.argbHexString, 0f),
-                    Item("#FF02063E", 4f),
-                    Item("#FFF21E07", 5f),
-                    Item("#FFEDED0C", 1f),
-                    Item("#FFFCFEF0", 0.5f),
+                    Item(Color.Black.rgbHexString, 0f),
+                    Item("#02063E", 4f),
+                    Item("#F21E07", 5f),
+                    Item("#EDED0C", 1f),
+                    Item("#FCFEF0", 0.5f),
                 ),
             ),
             ColorPaletteDefinition(
                 name = "Midnight",
                 items = listOf(
-                    Item(Color.Black.argbHexString, 0f),
-                    Item("#FF1F1F47", 3f),
-                    Item("#FFC45A0F", 2f),
-                    Item("#FFF9CA3A", 0.5f),
-                    Item(White.argbHexString, 0.5f),
+                    Item(Color.Black.rgbHexString, 0f),
+                    Item("#1F1F47", 3f),
+                    Item("#C45A0F", 2f),
+                    Item("#F9CA3A", 0.5f),
+                    Item(White.rgbHexString, 0.5f),
+                ),
+            ),
+            ColorPaletteDefinition(
+                name = "Rainbow",
+                items = listOf(
+                    Item(Color.Black.rgbHexString, 0f),
+                    Item(Color.Blue.rgbHexString, 1f),
+                    Item(Color.Cyan.rgbHexString, 1f),
+                    Item(Color.Green.rgbHexString, 1f),
+                    Item(Color.Yellow.rgbHexString, 1f),
+                    Item(Color.Red.rgbHexString, 1f),
                 ),
             ),
         )
