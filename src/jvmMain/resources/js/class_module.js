321000bc242767df389b3c37780c5b280cb5a1eb
sdercolin <sder.colin@gmail.com>
2023-09-13 16:45:27 +0900

    Update js module definition

--- src/jvmMain/resources/js/class_module.js
+++ src/jvmMain/resources/js/class_module.js
@@ -1,11 +1,12 @@
 class Module {
-    constructor(name, sampleDirectory, entries, currentIndex, rawFilePath, entryFilter = null) {
+    constructor(name, sampleDirectory, entries, currentIndex, rawFilePath, entryFilter = null, extras = {}) {
         this.name = name
         this.sampleDirectory = sampleDirectory // absolute path
         this.entries = entries
         this.currentIndex = currentIndex // current entry index
         this.rawFilePath = rawFilePath // absolute path to the raw label file
         this.entryFilter = entryFilter // see the following class EntryFilter
+        this.extras = extras // extra information, defined in [LabelerConf.moduleExtraFields]
     }
 }
 
