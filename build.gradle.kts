db5879926b556d61545b641ec278ea99cceb4c70
sdercolin <sder.colin@gmail.com>
2023-10-13 08:15:10 +0900

    Upgrade ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization") version "1.8.10"
     id("org.jetbrains.compose")
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("com.github.jk1.dependency-license-report") version "2.0"
 }
 
