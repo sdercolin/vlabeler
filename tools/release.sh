711c87dfd46205cec6df76e773f459b4b0a4c7b4
sdercolin <sder.colin@gmail.com>
2023-10-22 16:50:32 +0900

    Update release flow to check version match

--- tools/release.sh
+++ tools/release.sh
@@ -2,6 +2,7 @@
 
 # Dynamically determine path to gradle.properties
 PROPERTIES_PATH="$(dirname "$0")/../gradle.properties"
+PROPERTIES_PATH=$(realpath "$PROPERTIES_PATH")
 
 # Check if at least a tag name is provided
 if [ "$#" -lt 1 ]; then
@@ -22,9 +23,29 @@ APP_VERSION=$(awk -F'=' '/^app.version/ {print $2}' $PROPERTIES_PATH)
 
 # Check if TAG_NAME matches APP_VERSION
 if [ "$TAG_NAME" != "$APP_VERSION" ]; then
-    echo "ERROR: The provided TAG_NAME ($TAG_NAME) does not match app.version ($APP_VERSION) in $PROPERTIES_PATH."
-    echo "Please update the gradle.properties file or provide the correct TAG_NAME."
-    exit 1
+    echo "The provided TAG_NAME ($TAG_NAME) does not match app.version ($APP_VERSION) in $PROPERTIES_PATH."
+    echo "Do you want to update app.version to $TAG_NAME? (y/n)"
+    read -r response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+        # Update app.version in gradle.properties
+        sed -i "" "s/^app.version=.*/app.version=$TAG_NAME/" $PROPERTIES_PATH
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Failed to update app.version in $PROPERTIES_PATH."
+            exit 1
+        fi
+        echo "Updated app.version to $TAG_NAME in $PROPERTIES_PATH."
+        # Commit the change
+        git add $PROPERTIES_PATH
+        git commit -m "Update app.version to $TAG_NAME"
+        git push
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Failed to commit app.version update."
+            exit 1
+        fi
+    else
+        echo "Aborting."
+        exit 1
+    fi
 fi
 
 # Set the tag to the current HEAD (forcefully if -f is provided)
