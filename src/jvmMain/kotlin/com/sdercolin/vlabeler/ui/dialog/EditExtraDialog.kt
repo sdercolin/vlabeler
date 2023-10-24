a8d49f2a6952272280534154c5d33f2c72b28ce0
sdercolin <sder.colin@gmail.com>
2023-10-24 23:08:50 +0900

    Fix layout of EditExtraDialog

--- src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/dialog/EditExtraDialog.kt
+++ src/jvmMain/kotlin/com/sdercolin/vlabeler/ui/dialog/EditExtraDialog.kt
@@ -1,17 +1,12 @@
 package com.sdercolin.vlabeler.ui.dialog
 
-import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -24,15 +19,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.sdercolin.vlabeler.env.isReleased
 import com.sdercolin.vlabeler.model.LabelerConf
 import com.sdercolin.vlabeler.ui.common.CancelButton
 import com.sdercolin.vlabeler.ui.common.ConfirmButton
-import com.sdercolin.vlabeler.ui.string.Strings
-import com.sdercolin.vlabeler.ui.string.string
+import com.sdercolin.vlabeler.ui.common.HeaderFooterColumn
+import com.sdercolin.vlabeler.ui.string.*
 
 data class EditExtraDialogArgs(
     val index: Int,
@@ -103,69 +97,65 @@ fun EditExtraDialog(
     val state = remember {
         EditExtraState(args, finish)
     }
-
-    Column(modifier = Modifier.width(400.dp).height(400.dp)) {
-        Spacer(Modifier.height(20.dp))
-        Text(
-            text = string(args.target.stringKey),
-            style = MaterialTheme.typography.body2,
-            fontWeight = FontWeight.Bold,
-        )
-        Spacer(Modifier.height(25.dp))
-        ExtraContent(state)
-        Spacer(Modifier.height(25.dp))
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-            CancelButton(state::cancel)
-            Spacer(Modifier.width(25.dp))
-            ConfirmButton(
-                enabled = state.isAllValid,
-                onClick = state::trySubmit,
+    HeaderFooterColumn(
+        modifier = Modifier.fillMaxWidth(0.6f).padding(top = 20.dp),
+        header = {
+            Text(
+                text = string(args.target.stringKey),
+                style = MaterialTheme.typography.h5,
             )
-        }
+            Spacer(Modifier.height(25.dp))
+        },
+        footer = {
+            Spacer(Modifier.height(25.dp))
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                CancelButton(state::cancel)
+                Spacer(Modifier.width(25.dp))
+                ConfirmButton(
+                    enabled = state.isAllValid,
+                    onClick = state::trySubmit,
+                )
+            }
+        },
+    ) {
+        Content(state)
     }
 }
 
 @Composable
-private fun ColumnScope.ExtraContent(
+private fun Content(
     state: EditExtraState,
 ) {
-    Row(modifier = Modifier.fillMaxWidth().weight(1f)) {
-        val scrollState = rememberScrollState()
-        Column(modifier = Modifier.weight(1f).verticalScroll(scrollState)) {
-            state.extraFields.forEachIndexed { index, extraField ->
-                if (extraField.isVisible) {
-                    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            text = extraField.displayedName.get(),
-                            style = MaterialTheme.typography.body2,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                        )
-                        Spacer(Modifier.width(25.dp))
-                        TextField(
-                            modifier = Modifier.weight(1f)
-                                .onKeyEvent {
-                                    if (it.isReleased(Key.Enter)) {
-                                        state.trySubmit()
-                                        true
-                                    } else {
-                                        false
-                                    }
-                                },
-                            value = state.getNotNull(index),
-                            onValueChange = { state.update(index, it.ifBlank { null }) },
-                            singleLine = true,
-                            isError = state.isValid(index).not(),
-                            enabled = extraField.isEditable,
-                        )
-                    }
-                }
+    state.extraFields.forEachIndexed { index, extraField ->
+        if (extraField.isVisible) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    modifier = Modifier.width(200.dp),
+                    text = extraField.displayedName.get(),
+                    style = MaterialTheme.typography.body2,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                Spacer(Modifier.width(25.dp))
+                TextField(
+                    modifier = Modifier.weight(1f)
+                        .onKeyEvent {
+                            if (it.isReleased(Key.Enter)) {
+                                state.trySubmit()
+                                true
+                            } else {
+                                false
+                            }
+                        },
+                    value = state.getNotNull(index),
+                    onValueChange = { state.update(index, it.ifBlank { null }) },
+                    singleLine = true,
+                    isError = state.isValid(index).not(),
+                    enabled = extraField.isEditable,
+                )
             }
         }
-
-        VerticalScrollbar(
-            adapter = rememberScrollbarAdapter(scrollState),
-            modifier = Modifier.align(Alignment.CenterVertically).width(15.dp),
-        )
     }
 }
