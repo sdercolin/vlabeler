2bb9cc78686af8017106d5bed19bd932dac95a86
Maiko <130257355+maiko3tattun@users.noreply.github.com>
2026-05-03 22:48:49 +0900

    Add DiffSinger Labeler (#72)
    * Add DiffSinger Labeler
    
    * change name, category, description
    
    ---------
    
    Co-authored-by: Maiko <maikotattun@yahoo.co.jp>

---
+++ resources/common/labelers/diffsinger-labeler/projectConstructor.js
@@ -0,0 +1,25 @@
+// `root` is the input File object indicating the root sample directory.
+let wavFolder = root.resolve("")
+let labFolder = root.resolve("")
+let modules = []
+wavFolder.listChildFiles().filter(f => f.getExtension() === "wav").forEach(wavFile => {
+    let name = wavFile.getNameWithoutExtension()
+    let labFile = labFolder.resolve(name + ".lab")
+    modules.push(
+            new ModuleDefinition(
+                    name,
+                    wavFolder.getAbsolutePath(),
+                    [wavFile.getName()],
+                    [labFile.getAbsolutePath()],
+                    labFile.getAbsolutePath()
+            )
+    )
+})
+if (modules.length === 0) {
+    error({
+        en: 'No wav files found in the wav folder, please check the wav folder name in labeler settings.',
+        zh: 'wav 文件夹中没有找到 wav 文件，请检查标注器设置中的 wav 文件夹名称。',
+        ja: 'wav フォルダに wav ファイルが見つかりませんでした。ラベラーの設定で wav フォルダ名を確認してください。',
+        ko: 'wav폴더에 wav가 없습니다. 라벨러 설정에서 폴더 이름이 올바르게 되어 있는지 확인해 주세요.'
+    })
+}
